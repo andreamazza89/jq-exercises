d4710544faef94571046b9d64c66163326c41375
andrea mazzarella <andrea.mazzarella89@gmail.com>
2022-02-07 13:45:18 +0100

    refactors Interpreter test helper

--- test/Interpreter.purs
+++ test/Interpreter.purs
@@ -6,7 +6,6 @@ import Effect.Exception (Error)
 import Expression (Expression)
 import Helpers.Expression (accessByKeyNames, identity)
 import Interpreter (run) as Interpreter
-import Json (Json)
 import Json as Json
 import Prelude (Unit, discard)
 import Test.Spec (Spec, describe, it)
@@ -18,12 +17,12 @@ main = do
   describe "Interpreting" do
     describe "Identity" do
       it "identity" do
-        test2 identity
+        test identity
           "4.2"
           "4.2"
     describe "Accessor" do
       it "gets the value in the object at the given keys" do
-        test2 (accessByKeyNames [ "foo", "bar" ])
+        test (accessByKeyNames [ "foo", "bar" ])
           """
             {
               "foo": { "bar": "ciao" }
@@ -31,12 +30,10 @@ main = do
           """
           "\"ciao\""
 
-test :: forall a. MonadThrow Error a => Expression -> Json -> Json -> a Unit
-test expression input expectedOutput = Interpreter.run expression input `shouldEqual` (Right expectedOutput)
-
-test2 :: forall a. MonadThrow Error a => Expression -> String -> String -> a Unit
-test2 expression input expectedOutput = case [ runParser input Json.parser, runParser expectedOutput Json.parser ] of
-  [ Right i, Right o ] -> test expression i o
-  [ Left _, _ ] -> fail "failed to parse JSON in the input"
-  [ _, Left _ ] -> fail "failed to parse JSON in the output"
-  _ -> fail "failed to parse JSON"
+test :: forall a. MonadThrow Error a => Expression -> String -> String -> a Unit
+test expression input expectedOutput =
+  case [ parseJson input, parseJson expectedOutput ] of
+    [ Right i, Right o ] -> Interpreter.run expression i `shouldEqual` (Right o)
+    _ -> fail "failed to parse JSON"
+  where
+    parseJson s = runParser s Json.parser
