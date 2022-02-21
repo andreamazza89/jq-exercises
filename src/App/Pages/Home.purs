82dfe276435fd3374f0e796e2a2992688046477a
andrea mazzarella <andrea.mazzarella89@gmail.com>
2022-02-21 09:23:36 +0100

    moved HOme page to use reducer hook

--- src/App/Pages/Home.purs
+++ src/App/Pages/Home.purs
@@ -1,36 +1,65 @@
-module App.Pages.Home where
+module App.Pages.Home (mkHome) where
 
 import Prelude
-
-import Data.Maybe (maybe)
+import Data.Maybe (fromMaybe, maybe)
+import Effect (Effect)
 import JQ as JQ
 import React.Basic.DOM as DOM
 import React.Basic.DOM.Events (capture, targetValue)
-import React.Basic.Hooks (Component, component, useState, (/\))
+import React.Basic.Hooks (Component, Reducer, component, mkReducer, useReducer, useState, (/\))
 import React.Basic.Hooks as React
 
-type HomeProps = Unit
+type HomeProps
+  = Unit
+
+type State
+  = { jsonInput :: String
+    , expressionInput :: String
+    }
+
+data Action
+  = JsonInputUpdated String
+  | ExpressionInputUpdated String
+
+initialState :: State
+initialState =
+  { jsonInput: "{\"foo\": 42}"
+  , expressionInput: ".foo"
+  }
+
+reducerFn :: Effect (Reducer State Action)
+reducerFn =
+  mkReducer
+    ( \s action -> case action of
+        JsonInputUpdated newInput -> s { jsonInput = newInput }
+        ExpressionInputUpdated newInput -> s { expressionInput = newInput }
+    )
 
 mkHome :: Component HomeProps
 mkHome = do
-  component "Home" \_props -> React.do
+  reducer <- reducerFn
+  component "Home" \_ -> React.do
+    state' /\ dispatch <- useReducer initialState reducer
+    state /\ setState <- useState ({ input: "{\"foo\": 42}", exp: ".foo" })
+    pure
+      $ DOM.div
+          { children:
+              [ DOM.h1_ [ DOM.text "Jq - exercises" ]
+              , DOM.textarea
+                  { value: state'.jsonInput
+                  , onChange: inputChanged dispatch JsonInputUpdated
+                  }
+              , DOM.textarea
+                  { value: state'.expressionInput
+                  , onChange: inputChanged dispatch ExpressionInputUpdated
+                  }
+              , DOM.p_
+                  ( maybe ([ DOM.text "no result yet" ])
+                      (map DOM.text)
+                      (JQ.run state'.jsonInput state'.expressionInput)
+                  )
+              ]
+          }
 
-    counter /\ setCounter <- useState ({input: "{\"foo\": 42}", exp: ".foo"})
-
-    pure $ DOM.div
-      { children:
-          [ DOM.h1_ [ DOM.text "Jq - exercises" ]
-
-          , DOM.textarea {
-              value: counter.input,
-              onChange: capture targetValue (maybe (setCounter (identity)) (\a -> setCounter (\prev -> {input: a, exp: prev.exp})))
-            }
-
-          , DOM.textarea {
-              value: counter.exp,
-              onChange: capture targetValue (maybe (setCounter (identity)) (\a -> setCounter (\prev -> {input: prev.input, exp: a})))
-            }
-
-          , DOM.p_ (maybe ([DOM.text "no result yet"]) (map DOM.text) (JQ.run counter.input counter.exp ))
-          ]
-      }
+inputChanged dispatch buildAction =
+  capture targetValue (fromMaybe "does this ever happen?" >>> buildAction >>> dispatch)
