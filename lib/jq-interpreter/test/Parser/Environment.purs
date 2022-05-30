35130c6f1e5584a36942bb2de8c4854941e6df62
andrea mazzarella <andrea.mazzarella89@gmail.com>
2022-05-30 17:41:40 +0200

    adds broken test for error reporting when function definition is malformed

--- lib/jq-interpreter/test/Parser/Environment.purs
+++ lib/jq-interpreter/test/Parser/Environment.purs
@@ -2,6 +2,7 @@ module Test.Parser.Environment where
 
 import Prelude
 import Control.Monad.Error.Class (class MonadThrow)
+import Control.Monad.Writer (pass)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.Traversable (sequence)
@@ -10,11 +11,11 @@ import Effect.Exception (Error)
 import Environment (Environment, JqFunction)
 import Environment (empty, getFunction) as Env
 import Expression (Expression(..))
-import Helpers.Expression (identity) as Exp
 import Helpers.Expression ((~))
+import Helpers.Expression (identity) as Exp
 import Parser (parse)
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (fail, shouldEqual)
 
 main :: Spec Unit
 main = do
@@ -40,8 +41,17 @@ main = do
                   { body: Exp.identity ~ Exp.identity, environment: Env.empty }
               )
           )
+      it "a malformed definition should fail to parse" do
+        testEnvFailure
+          "def : .;"
+          "malformed environment"
 
 testEnvWith :: forall a b. MonadThrow Error a => Show b => Eq b => String -> (Environment Expression -> b) -> b -> a Unit
 testEnvWith expression modifyEnv expectedEnv = parsed `shouldEqual` (Right expectedEnv)
   where
   parsed = map (snd >>> modifyEnv) (parse expression)
+
+testEnvFailure :: forall a. MonadThrow Error a => String -> String -> a Unit
+testEnvFailure expression expectedErrorMsg = case parse expression of
+  Right _ -> fail "Should have failed to parse"
+  Left error -> expectedErrorMsg `shouldEqual` error
